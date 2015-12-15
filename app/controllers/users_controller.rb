6bf00aafcf7fc5227ed9430ec4db5baa15b3526a
AlexTaber <alex.taber0@gmail.com>
2015-12-15 15:35:51 -0500

    add sites/new view and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :password, :email)
+    params.require(:user).permit(:first_name, :last_name, :password, :email, :admin, :tier)
   end
 end
