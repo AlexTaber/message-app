f3897c1091e1099234a912bd2692de93ce142dda
AlexTaber <alex.taber0@gmail.com>
2015-12-15 15:03:16 -0500

    add users#create controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.valid?
+      @user.save
+      flash[:notice] = "User successfully created"
+      session[:user_id] = @user.id
+      redirect_to home_path
+    else
+      flash[:warn] = "Unable to create user, please try again"
+      redirect_to :back
+    end
   end
 
   def destroy
@@ -19,4 +30,7 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :password, :email)
+  end
 end
