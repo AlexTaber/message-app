79cf0f1b7be03a4f499fac96a53ee78d4f33221c
AlexTaber <alex.taber0@gmail.com>
2015-12-15 14:59:55 -0500

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :destroy]
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
