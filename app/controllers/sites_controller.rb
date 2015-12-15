1ce81c322983a0517741591b6dd0524174a82b81
AlexTaber <alex.taber0@gmail.com>
2015-12-15 15:57:11 -0500

    implement sites#create controller method

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,8 +4,30 @@ class SitesController < ApplicationController
   end
 
   def create
+    @site = Site.new(site_params)
+
+    if @site.valid?
+      @site.save
+      set_up_user
+      flash[:notice] = "Site successfully created"
+      redirect_to home_path
+    else
+      flash[:warn] = "Unable to create site, please try again"
+      redirect_to :back
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def site_params
+    params.require(:site).permit(:name, :url)
+  end
+
+  def set_up_user
+    @user = User.find_by(id: params[:site][:user_id])
+    UserSite.create(user_id: @user.id, site_id: @site.id)
   end
 end
