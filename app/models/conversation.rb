a250a86ae978dad3281ff7b133d39151353efa10
AlexTaber <alex.taber0@gmail.com>
2015-12-16 10:26:03 -0500

    add conversations#content_preview method

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,8 @@ class Conversation < ActiveRecord::Base
   has_many :conversers
   has_many :users, through: :conversers
   has_many :messages
+
+  def content_preview
+    messages.first.content_preview
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
+
+  def content_preview
+    content[0..20]
+  end
 end
