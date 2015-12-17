39186ad4424787e1fffb91506f46a2edab1d73fc
AlexTaber <alex.taber0@gmail.com>
2015-12-17 13:02:44 -0500

    add x-frame options

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module MessageApp
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
+
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
