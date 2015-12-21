65e5c13f8ec4dbaf23006cc47e0561cb34b64948
AlexTaber <alex.taber0@gmail.com>
2015-12-21 10:52:38 -0500

    update javascript structure to fix message form bug

---
+++ app/assets/javascripts/app/user_site.js
@@ -0,0 +1,15 @@
+//has to go on users site
+jQuery(document).ready(function($){
+    var opened = false
+    $( "#pwd-open-btn" ).click(function() {
+            $( "#fixed-iframe" )
+              .toggleClass("pwd-open");
+              if($( "#fixed-iframe" ).hasClass('pwd-open')) {
+                $('#pwd-open-btn').text('X');
+              } else {
+                $('#pwd-open-btn').text('---');
+              }
+              opened = true
+      });
+
+});
