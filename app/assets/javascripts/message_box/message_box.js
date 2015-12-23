3d5c45059fd345c3fa951f3f003595d0c17b86ce
intriligator <daniel@peekskillwebdesign.com>
2015-12-23 09:06:06 -0500

    submit on enter

--- app/assets/javascripts/message_box/message_box.js
+++ app/assets/javascripts/message_box/message_box.js
@@ -3,11 +3,19 @@ jQuery(document).ready(function($){
   msgBxDropdowns('#pwd-users', '#msg-bx-users-pwd');
   msgBxDropdowns('#pwd-sites', '#msg-bx-sites-pwd');
   msgBxDropdowns('#pwd-convos', '#msg-bx-convos-pwd');
+  enterSubmit('#message_content', '#new_message')
   $(".new_message").submit(sendMessage);
   $(".new_conversation").submit(startConversation);
   $('.msg-bx-convo-pwd').scrollTop($('.msg-bx-convo-pwd')[0].scrollHeight);
 });
-
+function enterSubmit(input, form) {
+  $(input).keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        $(form).submit();
+    }
+});
+}
 function msgBxDropdowns(clicked, target){
   $(clicked).on('click',function(){
     if($(this).hasClass('active')) {
