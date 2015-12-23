098a90fbadd09a70b4dde4be0cf36d5734504b9b
intriligator <daniel@peekskillwebdesign.com>
2015-12-23 12:06:46 -0500

    cleaned css

--- app/assets/javascripts/message_box/message_box.js
+++ app/assets/javascripts/message_box/message_box.js
@@ -1,12 +1,12 @@
 //our js
 jQuery(document).ready(function($){
-  msgBxDropdowns('#pwd-users', '#msg-bx-users-pwd');
-  msgBxDropdowns('#pwd-sites', '#msg-bx-sites-pwd');
-  msgBxDropdowns('#pwd-convos', '#msg-bx-convos-pwd');
+  msgBxDropdowns('#msg-bx-users-btn', '#msg-bx-users-drop');
+  msgBxDropdowns('#msg-bx-sites-btn', '#msg-bx-sites-drop');
+  msgBxDropdowns('#msg-bx-convos-btn', '#msg-bx-convos-drop');
   enterSubmit('#message_content', '#new_message')
   $(".new_message").submit(sendMessage);
   $(".new_conversation").submit(startConversation);
-  $('.msg-bx-convo-pwd').scrollTop($('.msg-bx-convo-pwd')[0].scrollHeight);
+  $('.msg-bx-convo').scrollTop($('.msg-bx-convo')[0].scrollHeight);
 });
 function enterSubmit(input, form) {
   $(input).keypress(function(event) {
@@ -19,10 +19,10 @@ function enterSubmit(input, form) {
 function msgBxDropdowns(clicked, target){
   $(clicked).on('click',function(){
     if($(this).hasClass('active')) {
-      $('.msg-bx-dropdown-pwd').slideUp();
+      $('.msg-bx-dropdown').slideUp();
       $(this).toggleClass('active');
     } else {
-      $('.msg-bx-dropdown-pwd').slideUp();
+      $('.msg-bx-dropdown').slideUp();
       $('i.active').removeClass('active')
       $(target).slideToggle();
       $(this).toggleClass('active');
@@ -37,7 +37,7 @@ function sendMessage(e) {
     method: "POST",
     data: $(e.target).serialize()
   }).done(function(response){
-    $(".msg-bx-convo-pwd").append(response);
+    $(".msg-bx-convo").append(response);
     $(".new_message").find("#message_content").val("");
     scrollToBottom();
   });
@@ -50,13 +50,13 @@ function startConversation(e) {
     method: "POST",
     data: $(e.target).serialize()
   }).done(function(response){
-    $(".msg-bx-convo-pwd").append(response);
+    $(".msg-bx-convo").append(response);
     $(".new_conversation").find("#content").val("");
     scrollToBottom();
   });
 }
-
 function scrollToBottom() {
-  var tar = $(".msg-bx-convo-pwd");
+  var tar = $(".msg-bx-convo");
   tar.scrollTop(tar[0].scrollHeight - tar.height())
 }
+
