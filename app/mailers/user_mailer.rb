61d6cc74bd08f4972ffafe2fac2286e361d6e460
AlexTaber <alex.taber0@gmail.com>
2016-01-12 11:00:39 -0500

    update url for emails to point to domain

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'info@peekskillwebdesign.com'
 
   def invite_email(invite)
-    @url  = "http://localhost:3000/users/new?invite_token=#{invite.token}"
+    @url  = "https://www.mercuryapp.co/users/new?invite_token=#{invite.token}"
     @site = invite.site
     @user = invite.user
     mail(to: invite.email, subject: "You've just been invited!")
