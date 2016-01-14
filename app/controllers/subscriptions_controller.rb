81eac6ef7748c92ce6b359b79743d0869d567731
AlexTaber <alex.taber0@gmail.com>
2016-01-14 12:03:49 -0500

    set tier following completed subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,11 +1,12 @@
 class SubscriptionsController < ApplicationController
   def new
     @subscription = Subscription.new
+    @tier_id = params[:tier_id]
   end
 
   def create
     @subscription = Subscription.new(subscription_params)
-    if @subscription.save_with_payment
+    if @subscription.save_with_payment(params[:tier_id].to_i)
       flash[:notice] = "Thank you for subscribing!"
       redirect_to home_path
     else
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,13 +3,15 @@ class Subscription < ActiveRecord::Base
 
   attr_accessor :stripe_card_token
 
-  def save_with_payment
+  def save_with_payment(tier_id)
     if valid?
-      customer = Stripe::Customer.create(description: user.email, plan: user.tier.id - 1, card: stripe_card_token)
+      customer = Stripe::Customer.create(description: user.email, plan: tier_id - 1, card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
+      user.update_attribute(:tier_id, tier_id)
     end
   rescue Stripe::InvalidRequestError => e
+    user.update_attribute(:tier_id, 1)
     logger.error "Stripe error while creating customer: #{e.message}"
     errors.add :base, "There was a problem with your credit card."
     false
