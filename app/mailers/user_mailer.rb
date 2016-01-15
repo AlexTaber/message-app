c2f98d3f004df00aa6f862692ee67e8146abdac6
AlexTaber <alex.taber0@gmail.com>
2016-01-15 15:07:58 -0500

    update email urls

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,7 +16,7 @@ class UserMailer < ApplicationMailer
   end
 
   def invite_email(invite)
-    @url  = "https://www.mercuryapp.co/users/new?invite_token=#{invite.token}"
+    @url  = "www.mercuryapp.co/users/new?invite_token=#{invite.token}"
     @site = invite.site
     @user = invite.user
     mail(to: invite.email, subject: "You've just been invited!", from: "Mercury Invite <admin@mercuryapp.co>")
