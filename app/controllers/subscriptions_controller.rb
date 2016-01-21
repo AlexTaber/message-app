191101d8541b71e4cfe4f784014befd61c1559be
AlexTaber <alex.taber0@gmail.com>
2016-01-21 16:05:04 -0500

    implement upgrades

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -15,6 +15,37 @@ class SubscriptionsController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if params[:tier_id]
+      if params[:tier_id].to_i > 1
+        if current_user.subscription.upgrade(params[:tier_id].to_i)
+          current_user.update_attributes(tier_id: params[:tier_id])
+          flash[:notice] = "Subscription successfully updated."
+          redirect_to home_path
+        else
+          flash[:warn] = "Unable to process new subscription plan, please try again"
+          redirect_to :back
+        end
+      else
+        if current_user.subscription.cancel
+          current_user.update_attributes(tier_id: params[:tier_id])
+          flash[:notice] = "Your account has been set to the Basic tier"
+          redirect_to home_path
+        else
+          flash[:warn] = "Unable to set tier to Basic, please try again"
+          redirect_to :back
+        end
+      end
+    else
+      flash[:warn] = "Please enter the tier you with to change to"
+      redirect_to :back
+    end
+  end
+
   def destroy
   end
 
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -9,12 +9,38 @@ class Subscription < ActiveRecord::Base
     if valid?
       customer = Stripe::Customer.create(description: user.email, plan: tier_id - 1, card: stripe_card_token)
       self.stripe_customer_token = customer.id
+      self.stripe_subscription_token = customer.subscriptions.first.id
       save!
       user.update_attribute(:tier_id, tier_id)
     end
   rescue Stripe::InvalidRequestError => e
     user.update_attribute(:tier_id, 1)
     logger.error "Stripe error while creating customer: #{e.message}"
+    errors.add :base, "There was a problem with your credit card."
+    false
+  end
+
+  def upgrade(tier_id)
+    customer = Stripe::Customer.retrieve(stripe_customer_token)
+    subscription = customer.subscriptions.retrieve(stripe_subscription_token)
+    subscription.plan = tier_id - 1
+    subscription.save
+    self.stripe_subscription_token = subscription.id
+
+  rescue Stripe::InvalidRequestError => e
+    user.update_attribute(:tier_id, 1)
+    logger.error "Stripe error while updating customer: #{e.message}"
+    errors.add :base, "There was a problem with your credit card."
+    false
+  end
+
+  def cancel
+    customer = Stripe::Customer.retrieve(stripe_customer_token)
+    subscription = customer.subscriptions.retrieve(stripe_subscription_token).delete(true)
+    self.stripe_subscription_token = nil
+  rescue Stripe::InvalidRequestError => e
+    user.update_attribute(:tier_id, 1)
+    logger.error "Stripe error while updating customer: #{e.message}"
     errors.add :base, "There was a problem with your credit card."
     false
   end
