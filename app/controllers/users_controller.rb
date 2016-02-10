bc39628aba6a6d47500c2c84673bfd8b187a743d
AlexTaber <alex.taber0@gmail.com>
2016-02-10 11:11:59 -0500

    add before_action require_permitted_user to users#home, #message_box

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :user_by_id, only: [:edit, :update]
+  before_action :require_permitted_user, only: [:home, :message_box]
 
   def new
     @default_tier_id = params[:default_tier_id] || 1
