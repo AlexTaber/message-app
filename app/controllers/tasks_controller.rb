c50eb32c0c19a5c528be7d844973051b5cb93b67
AlexTaber <alex.taber0@gmail.com>
2016-02-23 11:37:28 -0500

    tasks can be untaskified in coversations mode in real time

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
     if @task.valid?
       @task.save
       flash[:notice] = "Task successfully created"
-      fire_pusher_event(true)
+      fire_pusher_event(@task, @task.message, @task.message.conversation.users, true)
     else
       flash[:warn] = "Unable to create task"
     end
@@ -21,7 +21,7 @@ class TasksController < ApplicationController
     if @task.valid?
       @task.save
       flash[:notice] = "Task successfully updated"
-      fire_pusher_event(false)
+      fire_pusher_event(@task, @task.message, @task.message.conversation.users, false)
     else
       flash[:warn] = "Unable to update task"
     end
@@ -31,8 +31,11 @@ class TasksController < ApplicationController
 
   def destroy
     if @task
+      message = @task.message
+      users = @task.message.conversation.users
       @task.delete
       flash[:notice] = "Task Deleted"
+      fire_pusher_event(nil, message, users, false)
     else
       flash[:warn] = "No task by that id"
     end
@@ -50,20 +53,20 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: params[:id])
   end
 
-  def fire_pusher_event(new_record)
-    @task.message.conversation.users.each do |user|
-      Pusher.trigger("task#{@task.message.conversation.token}#{user.id}", 'new-task', {
-        message_id: @task.message.id,
-        user_id: user.id,
-        conversation_token: @task.message.conversation.token,
-        current_user_html: (render_to_string partial: "messages/current_user_message", locals: { message: @task.message, task: @task }),
-        other_user_html: (render_to_string partial: "messages/other_user_message", locals: { message: @task.message, task: @task }),
-        task_html: task_html(new_record)
+  def fire_pusher_event(task, message, users, new_record)
+    users.each do |user|
+      Pusher.trigger("task#{message.conversation.token}#{user.id}", 'new-task', {
+        message_id: message.id,
+        user_id: message.user.id,
+        conversation_token: message.conversation.token,
+        current_user_html: (render_to_string partial: "messages/current_user_message", locals: { message: message, task: task }),
+        other_user_html: (render_to_string partial: "messages/other_user_message", locals: { message: message, task: task }),
+        task_html: task_html(task, new_record)
       })
     end
   end
 
-  def task_html(new_record)
-    new_record ? (render_to_string partial: "tasks/task", locals: { task: @task } ) : ""
+  def task_html(task, new_record)
+    new_record ? (render_to_string partial: "tasks/task", locals: { task: task } ) : ""
   end
 end
