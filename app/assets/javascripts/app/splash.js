00c73697f62823c4b224e0294aa79c073df403f5
AlexTaber <alex.taber0@gmail.com>
2016-05-13 15:56:41 -0400

    redesign splash

--- app/assets/javascripts/app/splash.js
+++ app/assets/javascripts/app/splash.js
@@ -1,87 +1,65 @@
-var curTab = -1;
-var tabColors = [
-  "#455372",
-  "#1379be"
-]
-var totalTabs;
 var scrollDiv;
 var navTransitioned = false;
-var transitionTime = 5000;
-var nextEvent;
 var canTransition = true;
 var curTransition = 0;
 var curTransitionEl;
 var transitionOffset = $(window).height() * 0.75;
+
+var celebrationTextArr = [
+  "Time to Party :)",
+  "*danceparty*",
+  "Eat Some Cake :)",
+  "Happytimes Abound :)",
+  "Celebrate With Friends :)",
+  "Do a Dance :)",
+  "Bask in Your Accomplishments :)"
+]
+var landingTextArr = [
+  "Let's fly to the moon!",
+  "Go save the world!",
+  "Send me that image!",
+  "Can you help me move??",
+  "Don't forget to bring cake!"
+]
+var headerTextArr = [
+  "Send Your Team a Message",
+  "Mark It as a Task",
+  "Complete It!",
+  false,
+  "Message-Based Task Management"
+]
+var landingText;
+var landingTextEl;
+var mainEl;
+var landingTextIndex = 0;
+var headerTextIndex = 0;
+var events = [
+  transition1,
+  transition2,
+  transition3,
+  transition4,
+  transition5,
+  transition6,
+  transition7
+]
+var eventIndex = 0;
 
 window.addEventListener ?
 window.addEventListener("load",splashLoad,false) :
 window.attachEvent && window.attachEvent("onload",splashLoad);
 
 function splashLoad() {
-  totalTabs = $(".landing-tab").length;
+  var splashPage = $(".landing").length;
   totalTransitions = $(".transitionable").length;
 
-  if(totalTabs > 0) {
-    //setup tabs
+  if(splashPage > 0) {
     setTimeout(function() {
       $("#ajax-loader").fadeOut(500);
-      nextTab();
+      setUpLanding();
       setUpNav();
     }, 200);
-
-    //setup arrows
-    $("#right-arrow").on('click', nextTab);
-    $("#left-arrow").on('click', prevTab);
-
-    //setup circles
-    $(".circle").on('click', clickCircle);
   }
 };
-
-function setUpTabs() {
-  nextEvent = setTimeout(function() {
-    nextTab();
-  }, transitionTime);
-}
-
-function nextTab() {
-  var newTab = (curTab + 1) % totalTabs;
-  setTab(newTab);
-}
-
-function prevTab() {
-  var newTab = curTab - 1;
-  if (newTab < 0) { newTab = totalTabs - 1; }
-  setTab(newTab);
-}
-
-function setTab(newTab) {
-  var originalTab = curTab;
-
-  if(canTransition) {
-    canTransition = false;
-    $("#tab" + String(originalTab)).removeClass("is-active");
-    setTimeout(function() {
-      $(".navbar").removeClass("tab" + String(originalTab));
-      $(".arrow").removeClass("tab" + String(originalTab));
-      $("#circle" + String(originalTab)).removeClass("tab" + String(originalTab));
-      $("#circle" + String(originalTab)).removeClass("is-active");
-
-      $(".navbar").addClass("tab" + String(newTab));
-      $(".arrow").addClass("tab" + String(newTab));
-      $("#circle" + String(newTab)).addClass("tab" + String(newTab));
-      $("#circle" + String(newTab)).addClass("is-active");
-
-      $("#tab" + String(newTab)).addClass("is-active");
-      canTransition = true;
-    }, 800);
-
-    curTab = newTab;
-    transitionTime = 8000;
-    clearTimeout(nextEvent);
-    setUpTabs();
-  }
-}
 
 function setUpNav() {
   scrollDiv = $(".scroll-div");
@@ -91,14 +69,12 @@ function setUpNav() {
 function scrollNav() {
   var top = scrollDiv.scrollTop();
 
-  if(top > 500) {
+  if(top > 400) {
     if(!navTransitioned) {
       transitionNav();
-      clearTimeout(nextEvent);
     }
   } else if(navTransitioned) {
     untransitionNav();
-    setUpTabs();
   }
 }
 
@@ -112,7 +88,131 @@ function untransitionNav() {
   navTransitioned = false;
 }
 
-function clickCircle() {
-  var id = $(this).attr('id').slice(-1);
-  setTab(parseInt(id));
+function setUpLanding() {
+  mainEl = $(".main-object");
+  landingTextEl = mainEl.children("p");
+  setLandingText();
+}
+
+function setLandingText() {
+  landingText = landingTextArr[Math.floor(Math.random() * landingTextArr.length)];
+
+  setNextType();
+}
+
+function typeLandingText() {
+
+  if(landingTextIndex < landingText.length) {
+    var character = landingText[landingTextIndex];
+
+    landingTextEl.append(character);
+    setNextType();
+  } else {
+    setTimeout(function() {
+      nextEvent();
+    }, 500);
+  }
+
+  landingTextIndex += 1;
+}
+
+function setNextType() {
+  setTimeout(function() {
+    typeLandingText();
+  }, 150);
+}
+
+function nextEvent() {
+  events[eventIndex]();
+
+  eventIndex += 1;
+}
+
+function transition1() {
+  $(".send").addClass("tran1");
+  setTimeout(function() {
+    $(".send").addClass("tran2");
+    $(".send").removeClass("tran1");
+
+    setTimeout(function() {
+      nextEvent();
+    }, 200);
+
+  }, 200)
+}
+
+function transition2() {
+  mainEl.addClass("tran1");
+  $("#set-1").addClass("tran1");
+  nextHeaderText();
+
+  setTimeout(nextEvent, 2000);
+}
+
+function transition3() {
+  $("#set-1").addClass("tran2");
+  setTimeout(function() {
+    $("#set-1").removeClass("tran2");
+
+    setTimeout(function() {
+      nextEvent();
+    }, 200);
+
+  }, 200)
+}
+
+function transition4() {
+  $("#set-1").removeClass("tran1");
+  $("#set-2").addClass("tran1");
+  mainEl.addClass("tran2");
+  nextHeaderText();
+
+  setTimeout(nextEvent, 2000);
+}
+
+function transition5() {
+  $("#set-2").addClass("tran2");
+  setTimeout(function() {
+    $("#set-2").removeClass("tran2");
+
+    setTimeout(function() {
+      nextEvent();
+    }, 200);
+
+  }, 200)
+}
+
+function transition6() {
+  mainEl.removeClass("tran2");
+  mainEl.addClass("tran3");
+  $("#set-2").removeClass("tran1");
+  nextHeaderText();
+
+  setTimeout(nextEvent, 3000);
+}
+
+function transition7() {
+  $(".landing-content").addClass("tran1");
+  $(".landing-logo").addClass("tran1");
+  nextHeaderText();
+}
+
+function nextHeaderText() {
+  var textEl = $(".landing-text").children("h1");
+  textEl.fadeOut(500);
+  setTimeout(function() {
+    headerTextIndex += 1;
+    var text = headerTextArr[headerTextIndex];
+
+    if(!text) {
+      text = getCelebrationText();
+    }
+
+    textEl.html(text)
+    textEl.fadeIn(800);
+  }, 500)
+}
+
+function getCelebrationText() {
+  return celebrationTextArr[Math.floor(Math.random() * celebrationTextArr.length)];
 }
