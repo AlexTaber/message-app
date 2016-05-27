55b79fee66472eb819b4cd3e1c96853385135967
AlexTaber <alex.taber0@gmail.com>
2016-05-27 14:41:52 -0400

    implement colorful form fanciness

--- app/assets/javascripts/app/home.js
+++ app/assets/javascripts/app/home.js
@@ -8,6 +8,7 @@ var totalTransitions;
 var invalidFiles = [];
 var searchMode = false;
 var canSubmitProject = true;
+var formWrapper;
 
 
 jQuery(document).ready(function($){
@@ -89,6 +90,9 @@ jQuery(document).ready(function($){
 
   //set up tabs
   tabs();
+
+  //set up message form color change
+  setUpFormColor();
 
   //prevent double submit for projects
   preventProjectDoubleSubmit();
@@ -1305,3 +1309,41 @@ function preventProjectDoubleSubmit() {
     }
   })
 }
+
+function setUpFormColor() {
+  $("#message_content")[0].addEventListener("keyup", changeFormColor);
+}
+
+function changeFormColor() {
+  var length = $(this).val().length;
+  var maxLength = 50;
+  var firstColor = "f2f6f9";
+  var lastColor = "C0C1DC";
+
+  var newColor = mix(firstColor, lastColor, 100 - (Math.min(1, length / maxLength) * 100));
+
+  $("#form-wrapper").css("background", newColor);
+}
+
+var mix = function(color_1, color_2, weight) {
+  function d2h(d) { return d.toString(16); }  // convert a decimal value to hex
+  function h2d(h) { return parseInt(h, 16); } // convert a hex value to decimal
+
+  weight = (typeof(weight) !== 'undefined') ? weight : 50; // set the weight to 50%, if that argument is omitted
+
+  var color = "#";
+
+  for(var i = 0; i <= 5; i += 2) { // loop through each of the 3 hex pairs—red, green, and blue
+    var v1 = h2d(color_1.substr(i, 2)), // extract the current pairs
+        v2 = h2d(color_2.substr(i, 2)),
+
+        // combine the current pairs from each source color, according to the specified weight
+        val = d2h(Math.floor(v2 + (v1 - v2) * (weight / 100.0)));
+
+    while(val.length < 2) { val = '0' + val; } // prepend a '0' if val results in a single digit
+
+    color += val; // concatenate val to our new color string
+  }
+
+  return color; // PROFIT!
+};
